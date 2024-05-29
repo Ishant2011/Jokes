8bb62785e48da016618d3024fc1341a9f2cabd29
Ishant2011 <chhayanksingh22@gmail.com>
2024-05-29 08:50:56 +0530

    added index.html, style.css and script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const a = document.getElementById("sss")
+a.style.color = "cyan"
+a.style.background = "black"
+a.style.border = "3px solid cyan"
+
+// let a = document.getElementsByClassName("h1")[0]
+// a.style.color = "red"
+
+const b = document.body.getElementsByClassName("p")
+// b.style.font-size = "20px"
